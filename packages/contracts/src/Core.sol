8a1c565d404069eb74ae02f8227d417189687e3e
HemaDeviU <uhemadevi1927@gmail.com>
2024-09-21 21:04:57 +0800

    minor change

--- packages/contracts/src/Core.sol
+++ packages/contracts/src/Core.sol
@@ -5,7 +5,7 @@ import "./Strategy.sol";
 
 contract Core {
     mapping(address => address) public userStrategies;
-    event StrategyDeployed(address user, address StrategyContract);
+    event StrategyDeployed(address indexed user, address indexed StrategyContract);
     constructor(){}
 
     function createMyStrategy(address destinationWallet, uint256 destinationChain) external
@@ -14,5 +14,9 @@ contract Core {
         Strategy strategy = new Strategy(msg.sender,destinationWallet, destinationChain);
         userStrategies[msg.sender] = address(strategy);
         emit StrategyDeployed(msg.sender, address(strategy));
+    }
+    function getStrategy(address user) public returns (address)
+    {
+        return userStrategies[user];
     }
 }
